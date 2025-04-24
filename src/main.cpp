4efe186af5deff3760b14cc8a2af9c705e24f837
Piotr <piotrek124@hotmail.com>
2025-04-24 14:18:39 +0200

    Battery and identity

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,13 @@
 #include <ArduinoMqttClient.h>
 #include <ESP32Servo.h>
 
-
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
+
+//Identity
+const char* DEVICE_TYPE = "Lock";
+const char* DEVICE_NAME = "Smart Door Lock";
+String DEVICE_ID;
 
 //Wifi
 // const char* ssid = "Piotr";
@@ -23,8 +27,29 @@ const char* mqttPassword = "admin";
 //Servo
 Servo servo;
 
+//Batery
+int BAT = 35;
+unsigned long last_battery_check;
+
 WebServer server(80);
 
+//Identity
+String getChipId() {
+    uint64_t chip_id = ESP.getEfuseMac();
+    return String(chip_id, HEX);
+}
+String getIdentityJson() {
+    return  "{ \"id\": \""+DEVICE_ID+"\", \"name\": \""+DEVICE_NAME+"\", \"type\": \""+DEVICE_TYPE+"\" }";
+}
+void initIdentity() {
+    DEVICE_ID = getChipId();
+    Serial.print("Device ID: ");
+    Serial.println(DEVICE_ID);
+    Serial.println(getIdentityJson());
+}
+
+
+//Server
 void handleRoot() {
     server.send(200, "text/plain", "ESP32-S3 is running!");
 }
@@ -48,6 +73,17 @@ void loopServer() {
     server.handleClient();
 }
 
+//MQTT
+void publishMQTT(String topic, String payload) {
+    Serial.print("Publishing to ");
+    Serial.print(topic);
+    Serial.print(" : ");
+    Serial.println(payload);
+
+    mqttClient.beginMessage(topic);
+    mqttClient.print(payload);
+    mqttClient.endMessage();
+}
 void onMQTTMessage(int messageSize) {
     String message = "";
     String topic = mqttClient.messageTopic();
@@ -57,18 +93,13 @@ void onMQTTMessage(int messageSize) {
         message += c;
     }
 
-    Serial.print("Received message on topic: ");
-    Serial.println(topic);
-    Serial.println("Payload: " + message);
-
-
-    if (topic == "servo/rotate") {
+    if (topic == "lock/open") {
         int angle = message.toInt();
 
         servo.write(angle);
         Serial.println("Rotating servo");
 
-        delay(3000);
+        delay(1500);
         Serial.println("Reversing servo");
         servo.write(0);
 
@@ -78,6 +109,26 @@ void onMQTTMessage(int messageSize) {
         // digitalWrite(12, HIGH);
         // delay(angle * 10);
         // digitalWrite(12, LOW);
+    }
+    else if (topic == "lock/get/battery") {
+        int ADC = analogRead(BAT);
+        last_battery_check = millis();
+
+        Serial.print("Battery voltage: ");
+        Serial.println(ADC);
+
+        publishMQTT("debug/debug", String(ADC));
+    }
+    else if (topic == "lock/get/rssi") {
+        int rssi = WiFi.RSSI();
+
+        Serial.print("RSSI: ");
+        Serial.println(rssi);
+
+        publishMQTT("debug/debug", String(rssi));
+    }
+    else if (topic == "presence") {
+        publishMQTT("presence/confirm", getIdentityJson());
     }
 
 }
@@ -94,8 +145,10 @@ void initMQTT() {
     }
 
     Serial.println("Connected to MQTT broker!");
-    mqttClient.subscribe("test/topic");
-    mqttClient.subscribe("servo/rotate");
+    mqttClient.subscribe("lock/open");
+    mqttClient.subscribe("lock/get/battery");
+    mqttClient.subscribe("lock/get/rssi");
+    mqttClient.subscribe("presence");
     mqttClient.onMessage(onMQTTMessage);
 
 }
@@ -107,18 +160,32 @@ void loopMQTT() {
     mqttClient.poll();
 }
 
+//Servo
 void initServo() {
     servo.attach(16);
 }
 
+//Battery
+void loopBattery() {
+    int voltage = analogRead(BAT);
+
+    if (!last_battery_check || millis() - last_battery_check > 1000) {
+        last_battery_check = millis();
+
+        Serial.print("Battery voltage: ");
+        Serial.println(voltage);
+
+        // mqttClient.beginMessage("debug/debug");
+        // mqttClient.print(voltage);
+        // mqttClient.endMessage();
+    }
+}
 
 void setup() {
     delay(2000);
     Serial.begin(115200);
 
-    //TODO Debug
-    // pinMode(12, OUTPUT);
-
+    initIdentity();
     initServo();
     initServer();
     initMQTT();
@@ -127,4 +194,5 @@ void setup() {
 void loop() {
     loopServer();
     loopMQTT();
+    // loopBattery();
 }
